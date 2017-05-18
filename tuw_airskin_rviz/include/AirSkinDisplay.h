4af5883db36e88e653464ed84e62e2b131ba2236
Klaus Buchegger <klaus.buchegger@tuwien.ac.at>
2017-05-18 14:16:04 +0200

    Added airskin rviz plugin

---
+++ tuw_airskin_rviz/include/AirSkinDisplay.h
@@ -0,0 +1,123 @@
+/*
+ * Copyright (c) 2012, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef AIRSKIN_DISPLAY_H
+#define AIRSKIN_DISPLAY_H
+
+#ifndef Q_MOC_RUN
+#include <OgreVector3.h>
+#include <OgreQuaternion.h>
+#include <OgreSceneNode.h>
+#include <OgreSceneManager.h>
+#include <OgreEntity.h>
+#include <tuw_airskin_msgs/AirskinPressures.h>
+#include <tuw_airskin_msgs/AirskinColors.h>
+#include <ros/ros.h>
+#include "rviz/properties/ros_topic_property.h"
+#include "rviz/default_plugin/marker_display.h"
+#include <visualization_msgs/Marker.h>
+#include "rviz/default_plugin/markers/arrow_marker.h"
+#include "rviz/default_plugin/markers/shape_marker.h"
+#endif
+
+namespace Ogre
+{
+class SceneNode;
+}
+
+namespace rviz
+{
+class ColorProperty;
+class FloatProperty;
+class RosTopicProperty;
+class MarkerBase;
+class MarkerDisplay;
+}
+
+// All the source in this plugin is in its own namespace.  This is not
+// required but is good practice.
+namespace tuw_airskin_rviz
+{
+
+class AirSkinVisual;
+
+// Here we declare our new subclass of rviz::Display.  Every display
+// which can be listed in the "Displays" panel is a subclass of
+// rviz::Display.
+class AirSkinDisplay: public rviz::Display
+{
+    Q_OBJECT
+public:
+    // Constructor.  pluginlib::ClassLoader creates instances by calling
+    // the default constructor, so make sure you have one.
+    AirSkinDisplay();
+    virtual ~AirSkinDisplay();
+
+    // Overrides of protected virtual functions from Display.  As much
+    // as possible, when Displays are not enabled, they should not be
+    // subscribed to incoming data and should not show anything in the
+    // 3D view.  These functions are where these connections are made
+    // and broken.
+protected:
+    virtual void onInitialize();
+
+    // A helper to clear this display back to the initial state.
+    virtual void reset();
+
+    // These Qt slots get connected to signals indicating changes in the user-editable properties.
+private Q_SLOTS:
+    void updateTopics();
+private:
+    ros::NodeHandle nh_;
+    ros::Subscriber pressures_sub_;
+    ros::Subscriber colors_sub_;
+    void pressuresCallback(const tuw_airskin_msgs::AirskinPressures::ConstPtr& _pressures);
+    void colorsCallback(const tuw_airskin_msgs::AirskinColors::ConstPtr& _colors);
+    //Called periodically by rviz
+    void update( float wall_dt, float ros_dt );
+
+    rviz::RosTopicProperty* pressures_topic_;
+    rviz::RosTopicProperty* colors_topic_;
+    
+    // Storage of the visual
+    std::vector<std::shared_ptr<rviz::ArrowMarker>> arrows_;
+    std::vector<std::shared_ptr<rviz::ShapeMarker>> boxes_;
+    visualization_msgs::Marker defaultBoxMsg_;
+    visualization_msgs::Marker defaultArrowMsg_;
+    std::vector<visualization_msgs::Marker> arrowMsgs_;
+    rviz::MarkerDisplay markerDisplay_;
+    std::vector<unsigned int> min_press_;
+    std::vector<unsigned int> max_press_;
+    
+};
+
+} // end namespace tuw_pose_rviz_plugin
+
+#endif // LINES_SEGMENTS_2D_DISPLAY_H
+// %EndTag(FULL_SOURCE)%
---
+++ tuw_airskin_rviz/src/AirSkinDisplay.cpp
@@ -0,0 +1,159 @@
+/*
+ * Copyright (c) 2012, Willow Garage, Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Willow Garage, Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+#include "AirSkinDisplay.h"
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+
+namespace tuw_airskin_rviz
+{
+// The constructor must have no arguments, so we can't give the
+// constructor the parameters it needs to fully initialize.
+AirSkinDisplay::AirSkinDisplay()
+{
+    pressures_topic_ = new rviz::RosTopicProperty("Airskin Pressures Topic", "",
+                                         QString::fromStdString(ros::message_traits::datatype<tuw_airskin_msgs::AirskinPressures>()),
+                                         "tuw_airskin_msgs::AirskinPressures topic to subscribe to.", this, SLOT( updateTopics() ));
+    colors_topic_ = new rviz::RosTopicProperty("Airskin Colors Topic", "",
+                                         QString::fromStdString(ros::message_traits::datatype<tuw_airskin_msgs::AirskinColors>()),
+                                         "tuw_airskin_msgs::AirskinColors topic to subscribe to.", this, SLOT( updateTopics() ));
+}
+
+// After the top-level rviz::Display::initialize() does its own setup,
+// it calls the subclass's onInitialize() function.  This is where we
+// instantiate all the workings of the class.  We make sure to also
+// call our immediate super-class's onInitialize() function, since it
+// does important stuff setting up the message filter.
+//
+//  Note that "MFDClass" is a typedef of
+// ``MessageFilterDisplay<message type>``, to save typing that long
+// templated class name every time you need to refer to the
+// superclass.
+void AirSkinDisplay::onInitialize() {
+    defaultBoxMsg_.pose.orientation.w = 1.0;
+    defaultBoxMsg_.scale.x = 0.021;
+    defaultBoxMsg_.scale.y = 0.101;
+    defaultBoxMsg_.scale.z = 0.051;
+    
+    defaultArrowMsg_.pose.orientation.w = 1.0;
+    defaultArrowMsg_.color.a = 1.0;
+    defaultArrowMsg_.color.r = 1.0;
+    defaultArrowMsg_.color.g = 1.0;
+    defaultArrowMsg_.color.b = 1.0;
+    defaultArrowMsg_.scale.y = 0.025;
+    defaultArrowMsg_.scale.z = 0.025;
+}
+
+AirSkinDisplay::~AirSkinDisplay()
+{
+}
+
+// Clear the visual by deleting its object.
+void AirSkinDisplay::reset()
+{
+}
+
+// This is our callback to handle an incoming message.
+void AirSkinDisplay::update( float wall_dt, float ros_dt )
+{
+    //resize marker vectors and possibly fill if the message vector is of a different size
+    if(arrowMsgs_.size() > arrows_.size()) {
+         for(int i=0;i<(arrowMsgs_.size() - arrows_.size());i++) {
+            arrows_.emplace_back(std::make_shared<rviz::ArrowMarker>(&markerDisplay_, context_, scene_node_));
+            boxes_.emplace_back(std::make_shared<rviz::ShapeMarker>(&markerDisplay_, context_, scene_node_));
+         }
+    } else if(arrowMsgs_.size() <  arrows_.size()) {
+        arrows_.resize(arrowMsgs_.size());
+        boxes_.resize(arrowMsgs_.size());
+    }
+    
+    //pass messages to markers
+    //there is only one default box msg because the arrow msgs already contain all needed information (frame_id and color)
+    for(int i=0;i<arrows_.size();i++) {
+        arrows_[i]->setMessage(arrowMsgs_[i]);
+        visualization_msgs::Marker boxMsg;
+        boxMsg = defaultBoxMsg_;
+        boxMsg.color = arrowMsgs_[i].color;
+        boxMsg.header = arrowMsgs_[i].header;
+        boxes_[i]->setMessage(boxMsg);
+    }
+}
+
+void AirSkinDisplay::updateTopics() {
+    colors_sub_ = nh_.subscribe<tuw_airskin_msgs::AirskinColors>(colors_topic_->getTopicStd(), 1, &AirSkinDisplay::colorsCallback, this );
+    pressures_sub_ = nh_.subscribe<tuw_airskin_msgs::AirskinPressures>(pressures_topic_->getTopicStd(), 1, &AirSkinDisplay::pressuresCallback, this );
+}
+
+void AirSkinDisplay::colorsCallback(const tuw_airskin_msgs::AirskinColors::ConstPtr& _colors) {
+    for(int i=0;i<arrowMsgs_.size() && i<_colors->colors.size();i++) {
+        arrowMsgs_[i].color = _colors->colors[i];
+    }
+}
+
+void AirSkinDisplay::pressuresCallback(const tuw_airskin_msgs::AirskinPressures::ConstPtr& _pressures) {
+    //resize message vector and possibly fill if the received message contains a different amount of airskin measurements
+    if(arrowMsgs_.size()<_pressures->pressures.size()) {
+        for(int i=0;i<(_pressures->pressures.size() - arrowMsgs_.size());i++) {
+            arrowMsgs_.emplace_back(defaultArrowMsg_);
+            min_press_.emplace_back(UINT_MAX);
+            max_press_.emplace_back(0);
+        }
+    } else if(arrowMsgs_.size()>_pressures->pressures.size()) {
+        arrowMsgs_.resize(_pressures->pressures.size());
+        min_press_.resize(_pressures->pressures.size());
+        max_press_.resize(_pressures->pressures.size());
+    }
+    
+    //Update min and max measurements for scaling
+    for(int i=0;i<_pressures->pressures.size();i++) {
+        if(_pressures->pressures[i]<min_press_[i]) {
+            min_press_[i] = _pressures->pressures[i];
+        }
+        if(_pressures->pressures[i]>max_press_[i]) {
+            max_press_[i] = _pressures->pressures[i];
+        }
+    }
+    
+    //scale according to min and max measurements
+    //get frame id from pad name
+    for(int i=0;i<arrowMsgs_.size();i++) {
+        arrowMsgs_[i].scale.x = 0.02 + ((_pressures->pressures[i]-min_press_[i])/float(max_press_[i]+1000-min_press_[i]))/10;
+        arrowMsgs_[i].header.frame_id = tf::resolve("p3dx",_pressures->names[i]);
+    }
+}
+    
+    
+
+    
+
+}  // end namespace tuw_airskin_rviz_plugin
+
+// Tell pluginlib about this class.  It is important to do this in
+// global scope, outside our package's namespace.
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(tuw_airskin_rviz::AirSkinDisplay, rviz::Display)
